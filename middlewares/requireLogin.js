9dcd935656de0c65a5688dbcf05e3ac2cd15516d
adojem <adojemcovix@gmail.com>
2018-11-12 13:46:38 +0900

    Mongoose for Survey Creation

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,9 +1,9 @@
 module.exports = (req, res, next) => {
    if (!req.user) {
       return res.status(401).send({
-         error: 'You must log in!'
+         error: 'You must log in!',
       });
    }
 
-   next();
+   return next();
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,20 +1,16 @@
 const passport = require('passport');
 
-module.exports = app => {
+module.exports = (app) => {
    app.get(
       '/auth/google',
       passport.authenticate('google', {
-         scope: ['profile', 'email']
-      })
+         scope: ['profile', 'email'],
+      }),
    );
 
-   app.get(
-      '/auth/google/callback',
-      passport.authenticate('google'),
-      (req, res) => {
-         res.redirect('/surveys');
-      }
-   );
+   app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+      res.redirect('/surveys');
+   });
 
    app.get('/api/logout', (req, res) => {
       req.logout();
